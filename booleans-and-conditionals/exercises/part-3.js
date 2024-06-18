1505f7ff63f1a63df64d45c184d7764d7c3bf29d
Cody1117 <adamscody079@gmail.com>
2024-06-18 11:37:50 -0500

    updated booleans and conditionals exercises

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -1,6 +1,34 @@
 let engineIndicatorLight = 'red blinking';
-let fuelLevel = 21000;
-let engineTemperature = 1200;
+let fuelLevel = 900;
+let engineTemperature = 2500;
+let commandOverride = false;
+
+if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
+    console.log("ENGINE FAILURE IMMINENT!");
+  } else if (fuelLevel <= 5000 || engineTemperature > 2500){
+    console.log("Check fuel level. Engines running hot.");
+  } else if (fuelLevel > 20000 && engineTemperature <= 2500){
+    console.log("Full tank. Engines good.");
+  } else if (fuelLevel > 10000 && engineTemperature <= 2500){
+    console.log("Fuel level above 50%. Engines good.");
+  } else if (fuelLevel > 5000 && engineTemperature <= 2500){
+    console.log("Fuel level above 25%. Engines good.");
+  } else {
+    console.log("Fuel and engine status pending...");
+  }
+
+  if (commandOverride === false) {
+} else if (fuelLevel > 20000 && engineIndicatorLight != "red blinking"|| commandOverride === true){
+  console.log("Cleared to launch!")
+} else {
+  console.log("Launch scrubbed!")
+}
+
+
+
+if (commandOverride === true){
+  console.log("Suttle Clear for Launch!")
+}
 
 /* 5) Implement the following checks using if/else if/else statements:
 
