7ec829f13e76aed00f6b3d13b73e494df4755886
Cody1117 <adamscody079@gmail.com>
2024-06-20 11:21:19 -0500

    Completed Exersises Strings

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,35 @@
 //Create an array called practiceFile with the following entry: 273.15
+let jsCreator = "Brendan Eich";
 
+console.log(jsCreator[-1]);
+console.log(jsCreator[42]);
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+myStr = 'Index'
+myStr[6] === ' ';
+console.log(myStr[6])
 
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+
+cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;
+
+console.log(location.length);
+
+word = 'JavaScript';
+
+console.log (word.slice(4).toUpperCase())
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+let language = "JavaScript";
+language.replace('J', 'Q');
+language.slice(0,5);
+console.log(language);
+
+language = 'JavaScript';
+console.log(language.slice(1,6))
+
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,25 @@
 let num = 1001;
+let str = num.toString();
 
 //Returns 'undefined'.
-console.log(num.length);
+console.log(str.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+let numb = 123.45
+let strb = numb.toString();
 
+
+let lengthExcludingPeriod = strb.replace('.', '').length;
+
+console.log(lengthExcludingPeriod);
+console.log (strb.length)
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+console.log (strb.length)
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num).includes (".")) {
+    console.log(String(num).length-1);
+}   else {
+    console.log(String(num).length);
+}
+
+
