8989d6b3ab307c816ec5ea5c2e0c562c328c3710
Cody Adams <adamscody079@gmail.com>
2024-07-10 06:52:08 -0500

    Updated and finished exercises more on functions

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -1,5 +1,17 @@
 //Create an anonymous function and set it equal to a variable.
+let transform = function(item) {
+    if (typeof item === 'number') {
+        return item * 3;
+    } else if (typeof item === 'string') {
+        return "ARRR!";
+    } else {
+        return item;
+    }
+};
 
+console.log(transform(10)); // Should print 30
+console.log(transform("Hello")); // Should print "ARRR!"
+console.log(transform(true)); // Should print true
 /* Your function should:
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
@@ -13,3 +25,6 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+let transformedArray = arr.map(transform);
+
+console.log(transformedArray);
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -1,18 +1,18 @@
 function checkFuel(level) {
-  if (level > 100000){
+  if (level > 100000) {
     return 'green';
-  } else if (level > 50000){
+  } else if (level > 50000) {
     return 'yellow';
   } else {
     return 'red';
   }
 }
 
-function holdStatus(arr){
+function holdStatus(arr) {
   if (arr.length < 7) {
-    return `Spaces available: ${7-arr.length}.`;
-  } else if (arr.length > 7){
-    return `Over capacity by ${arr.length-7} items.`;
+    return `Spaces available: ${7 - arr.length}.`;
+  } else if (arr.length > 7) {
+    return `Over capacity by ${arr.length - 7} items.`;
   } else {
     return "Full";
   }
@@ -24,8 +24,44 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
+// Define anonymous function to siphon fuel
+let siphonFuel = function(fuelLevel) {
+  if (fuelLevel > 100000) {
+    return fuelLevel - 99999; // Reduce fuel just enough to stay green
+  } else if (fuelLevel > 50000) {
+    return fuelLevel - 49999; // Reduce fuel just enough to stay yellow
+  } else {
+    return fuelLevel; // Do not reduce further if it's already red
+  }
+};
+
+// Test the function
+let newFuelLevel = siphonFuel(fuelLevel);
+console.log("New Fuel Level: " + newFuelLevel); // Should print 100001
+console.log("Fuel level: " + checkFuel(newFuelLevel)); // Should still print "green"
+
+// Define anonymous function to swipe cargo
+let liberateCargo = function(cargoHold) {
+  let stolenItems = cargoHold.splice(0, 2, "worthless item 1", "worthless item 2");
+  return stolenItems;
+};
+
+// Test the function
+let stolenGoods = liberateCargo(cargoHold);
+console.log("Stolen Goods: " + stolenGoods); // Should print the first two items of the cargoHold
+console.log("Updated Cargo Hold: " + cargoHold); // Should show the cargoHold with two worthless items replacing the stolen ones
+
+// Define the irs function
+function irs(fuelLevel, cargoHold) {
+  let siphonedFuel = siphonFuel(fuelLevel);
+  let stolenItems = liberateCargo(cargoHold);
+  return `Raided ${fuelLevel - siphonedFuel} kg of fuel from the tanks, and stole ${stolenItems[0]} and ${stolenItems[1]} from the cargo hold.`;
+}
+
+// Test the irs function
+console.log(irs(fuelLevel, cargoHold)); // Should print the correct receipt
+
+
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
@@ -35,8 +71,8 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Decide where to best place your function call to gather our new fuel.
 
-/* Next, liberate some of that glorious cargo.
- * /
+// Next, liberate some of that glorious cargo.
+
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -46,8 +82,8 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Don’t get hasty, matey! Remember to test your function.
 
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
+// Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
+
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
