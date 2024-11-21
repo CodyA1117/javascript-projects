9164e9b603e93e272210698a5de66e04d0096a7e
CodyA1117 <adamscody079@gmail.com>
2024-11-21 08:14:12 -0600

    This is the completed version of exercise 16 with a working fetch.

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,37 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function() {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        jsonPromise = response.json();
+        jsonPromise.then( function(json) {
+        let container = document.getElementById("container");
+        let astronautList = '';
+        let hoursArray = [];
+        for (let i = 0; i < json.length; i++) {
+         hoursArray.push(json[i].hoursInSpace)
+        }
+        hoursArray.sort((a,b) => b-a);
+        console.log(hoursArray);
+        for (let i = 0; i < hoursArray.length; i++) {
+            for ( let j = 0; j < json.length; j++) {
+                console.log(json[j].hoursInSpace);
+            if (hoursArray[i] === json[j].hoursInSpace){
+                astronautList += `
+        <div class="astronaut">
+        <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+            <ul>
+            <li>${json[i].hoursInSpace}</li>
+            <li>Active: ${json[i].active}</li>
+            <li>Skills: ${json[i].skills}</li>
+            </ul>
+            </div>
+    <img class="avatar" src="${json[i].picture}">
+</div>
+            `
+            }
+        }
+    }
+        container.innerHTML = astronautList;
+        })
+    })
+})
