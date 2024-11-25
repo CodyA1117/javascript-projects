71496a4c9ed6a3f6dd4e42de237ea0548294a74a
CodyA1117 <adamscody079@gmail.com>
2024-11-25 06:54:23 -0600

    Updated bonus missions, and played with styling, and script.js file by adding bonus requirments

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,37 +1,39 @@
-//TODO: Add Your Code Below
-window.addEventListener("load", function() {
-    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-        jsonPromise = response.json();
-        jsonPromise.then( function(json) {
-        let container = document.getElementById("container");
-        let astronautList = '';
-        let hoursArray = [];
-        for (let i = 0; i < json.length; i++) {
-         hoursArray.push(json[i].hoursInSpace)
-        }
-        hoursArray.sort((a,b) => b-a);
-        console.log(hoursArray);
-        for (let i = 0; i < hoursArray.length; i++) {
-            for ( let j = 0; j < json.length; j++) {
-                console.log(json[j].hoursInSpace);
-            if (hoursArray[i] === json[j].hoursInSpace){
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            // Get the container element
+            let container = document.getElementById("container");
+
+            // Add the astronaut count
+            let astronautCount = document.createElement("h3");
+            astronautCount.textContent = `Total Astronauts: ${json.length}`;
+            container.appendChild(astronautCount);
+
+            // Sort astronauts by hours in space (descending)
+            json.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+
+            // Generate HTML for each astronaut
+            let astronautList = '';
+            for (let i = 0; i < json.length; i++) {
                 astronautList += `
-        <div class="astronaut">
-        <div class="bio">
-        <h3>${json[i].firstName} ${json[i].lastName}</h3>
-            <ul>
-            <li>${json[i].hoursInSpace}</li>
-            <li>Active: ${json[i].active}</li>
-            <li>Skills: ${json[i].skills}</li>
-            </ul>
-            </div>
-    <img class="avatar" src="${json[i].picture}">
-</div>
-            `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li style="color: ${json[i].active ? 'green' : 'black'};">
+                                Active: ${json[i].active}
+                            </li>
+                            <li>Skills: ${json[i].skills.join(', ')}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                </div>
+                `;
             }
-        }
-    }
-        container.innerHTML = astronautList;
-        })
-    })
-})
+
+            // Add the astronaut cards to the container
+            container.innerHTML += astronautList;
+        });
+    });
+});
